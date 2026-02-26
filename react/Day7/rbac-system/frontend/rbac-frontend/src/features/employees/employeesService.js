38101e840b293e4b78a5ff5ec3e466443e4416ea
Prafulla Purohit <prafullakalu@satvasolutions.com>
2026-02-26 19:03:32 +0530

    Delete react/Day7/rbac-system/frontend/rbac-frontend directory

--- react/Day7/rbac-system/frontend/rbac-frontend/src/features/employees/employeesService.js
+++
@@ -1,5 +0,0 @@
-import axiosInstance from "../../api/axiosInstance"
-
-export const createEmployee = (payload) => axiosInstance.post("/employees", payload)
-export const updateEmployee = (id, payload) => axiosInstance.patch(`/employees/${id}`, payload)
-export const deleteEmployee = (id) => axiosInstance.delete(`/employees/${id}`)
--- react/Day7/rbac-system/frontend/rbac-frontend/src/features/users/usersService.js
+++
@@ -1,5 +0,0 @@
-import axiosInstance from "../../api/axiosInstance"
-
-export const createUser = (payload) => axiosInstance.post("/users", payload)
-export const updateUser = (id, payload) => axiosInstance.patch(`/users/${id}`, payload)
-export const deleteUser = (id) => axiosInstance.delete(`/users/${id}`)
