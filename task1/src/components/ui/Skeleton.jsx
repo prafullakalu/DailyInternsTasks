800ece01486b0db0095256623c17fac04a6a6715
Prafulla Purohit <prafullakalu@satvasolutions.com>
2026-04-08 12:06:58 +0530

    Delete task1 directory

--- task1/src/components/ui/Skeleton.jsx
+++
@@ -1,7 +0,0 @@
-function Skeleton({ className }) {
-  return (
-    <div className={`bg-gray-300 dark:bg-gray-700 rounded ${className} animate-pulse`} />
-  )
-}
-
-export default Skeleton
--- task1/src/routes/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux"
-import { Navigate } from "react-router-dom"
-
-function ProtectedRoute({ children }) {
-  const user = useSelector(state => state.auth.user)
-  return user ? children : <Navigate to="/login" />
-}
-
-export default ProtectedRoute
